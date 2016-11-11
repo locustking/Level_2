c7d6462a8f8077d5f6f24c7c2af05c01b3f9f353
dcwalton2015 <dcwalton@sbcglobal.net>
2016-11-11 08:14:43 -0800

    plug in file

---
+++ bba_book_beat.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package BBA_DB
+ * @version 1.0
+ */
+/*
+Plugin Name: Book Beat
+Plugin URI: http://www.bookbeatapp.com
+Description: A tool for authors to do competitive analysis
+Author: Havard E-71, Fall 2016, Level 2 SCRUM tam
+Version: 1.0
+Author URI: http://www.bookbeatapp.com
+*/
+
+    // configuration
+        require dirname(__FILE__) . "/includes/bba_config.php"; 
+        require dirname(__FILE__) . "/includes/bba_helpers.php";
+        require dirname(__FILE__) . "/includes/bba_book_beat_class.php";
+
+    // Adds shortcode to call control loop from page
+    add_shortcode( 'bba_display', 'bookbeat_func' );
+
+function bookbeat_func($atts){
+    $book_title = "Programming with Cucumber";
+    $obj = new BookBeat;
+    $funcname = "setBookTitle";
+    $obj->$funcname($book_title);  
+    $funcname = "viewBookTitle";
+    $obj->$funcname($book_title); 
+}
+
+?>

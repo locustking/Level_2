53953f98ac09717727f94a6513219d22b5a0cde7
dcwalton2015 <dcwalton@sbcglobal.net>
2016-12-03 12:18:08 -0800

    returning to working state

--- features/bootstrap/bba_book_beat.php
+++ features/bootstrap/bba_book_beat.php
@@ -1,84 +1,32 @@
 <?php
 /**
  * @package BBA_DB
- * @version 2.0
+ * @version 1.0
  */
 /*
 Plugin Name: Book Beat
 Plugin URI: http://www.bookbeatapp.com
 Description: A tool for authors to do competitive analysis
 Author: Havard E-71, Fall 2016, Level 2 SCRUM tam
-Version: 2.0
+Version: 1.0
 Author URI: http://www.bookbeatapp.com
 */
-
-/*
-TODO:
-a. link up actual search
-b. add in tablesorter using enqueue
-*/
-
-   // display errors, warnings, and notices
-    ini_set("display_errors", true);
-    error_reporting(E_ALL);
 
     // configuration
         require dirname(__FILE__) . "/BookBeatList.php"; 
         require dirname(__FILE__) . "/BookBeatJSON.php";
         require dirname(__FILE__) . "/BookBeat.php";
-        require dirname(__FILE__) . "/bba_booklist_views.php";
 
     // Adds shortcode to call control loop from page
     add_shortcode( 'bba_display', 'bookbeat_func' );
 
 function bookbeat_func($atts){
-        // Check for POST method which means action, else render booklist view
-        if ($_SERVER["REQUEST_METHOD"] == "POST")
-        {
-            // Debug statement: 
-            echo "<span style='background-color: green'>POST from : " . $_POST['formtype'] . "</span>";
-            // determine what to do based on formtype of post (what happens if none?)
-            switch ($_POST['formtype'])
-            {
-                // clicked to edit booklist 
-                case 'buildlist' :
-                    $pagecontent = bba_searchform();
-                    break;
-                // redisplay booklist after editing it
-                case 'results' :
-                    $pagecontent = "<p>Results Updated</p>" . bba_booklist_display();
-                    break; 
-                // Added item to JSON
-                case 'addItem' :
-                   // will say something about item added to JSON
-                   break;
-            } 
-        }
-        // if nothing relevant, show the booklist
-        else
-        {
-            
-              $pagecontent = $content . bba_booklist_display();
-//            $values = array ("title"=>"Search");
-//            gaf_render("gaf_search_form.php", $values);
-        }
-            
-    // if post (from search form) do search and show results
-    
-    $pageheader = bba_pageheader();
-    $content = $pageheader . $pagecontent;
-    return $content;
- 
-}
+ //    Introduction Text    
+    $content = "<p>The following App was designed and pushed to our site by the Level 2 team. It is an initial piece of working software to prove we are able to work with external data, and display it on our site. We connect to Amazon's API and the following snippet displays information pulled by our code, to display statistics about the selected books we've chosen. Our github repo is located @";
+    $content = $content . "<a href = 'https://github.com/locustking/Level_2/' >https://github.com/locustking/Level_2/</a> - The implementation file can be found at our github at: <a href='https://github.com/locustking/Level_2/tree/master/features/bootstrap'>https://github.com/locustking/Level_2/tree/master/features/bootstrap</a></p><p>Click on the column heads to sort.";
 
-/*
-Displays the booklist and related data
-*/
-function bba_booklist_display(){
-
-
-    // Begin building tht table form
-    $content = "<div>";
+    // Table form
+    $content = $content . "<div>";
     $content = $content .  "<TABLE id='booklist' class='tablesorter {sortlist: [[2,0]]}'><THEAD><TR><TH>Title</TH><TH>Author</TH><TH>Sales Rank</TH><TH>Num Reviews</TH><TH>Avg Rating</TH></TR></THEAD><TBODY>";
     
     // Get Book Data
@@ -95,28 +43,26 @@ function bba_booklist_display(){
 
     // update bookbeatlist. this will collect the sales rank from amazon and update the json file.
     // returning an array with the updated list
-    $result = $bookbeatlist->updateSalesRank();
+	$source = "amazon";
+    $result = $bookbeatlist->updateSalesRank($source);
     
     // Display book list
     foreach ($result as $res){
-        $content = $content .  "<tr><td>" . $res->book_title . "</td><td>" . $res->author_name . "</TD><TD>" . $res->sales_rank . "</TD><TD>" . $res->num_reviews . "</TD><TD>" . $res->avg_ratings . "</TD></TR>";
+        $content = $content .  "<tr><td>" . $res->book_title . "</td><td>" . $res->author_name . "</TD><TD>" . $res->$source->sales_rank . "</TD><TD>" . $res->$source->num_reviews . "</TD><TD>" . $res->$source->avg_ratings . "</TD></TR>";
     }
         
 
      $content = $content . "</TBODY></TABLE></div>";
-
-//HOLD UNTIL WE IMPLEMENT THIS CORRECTLY
-//     $content = $content . "<script src=https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js'></script>
-//<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.26.0/js/jquery.tablesorter.min.js' type='text/javascript'> </script>
-//<script type='text/javascript'>
-//			$(document).ready(function()
-//			{
-//				$('#booklist').tablesorter();
-//			}
-//			);
-//		</script>";
+     $content = $content . "<script src=https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.26.0/js/jquery.tablesorter.min.js' type='text/javascript'> </script>
+<script type='text/javascript'>
+			$(document).ready(function()
+			{
+				$('#booklist').tablesorter();
+			}
+			);
+		</script>";
     return $content;
-    }
+}
 
 ?>
-

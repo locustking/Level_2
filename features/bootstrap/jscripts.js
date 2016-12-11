f515da78d39d71ba027c065d05dcb9fdbfea020b
Doug Walton <dcwalton@gmail.com>
2016-12-11 13:25:19 -0800

    Update js tablesorter code

--- features/bootstrap/jscripts.js
+++ features/bootstrap/jscripts.js
@@ -10,6 +10,10 @@ $('#dialog_link, ul#icons li').hover(
 function() { $(this).addClass('ui-state-hover'); },
 function() { $(this).removeClass('ui-state-hover'); }
 );
+
+// Tablesorters
+$("#booklist").tablesorter();
+$("#booklist2").tablesorter();
+$("#booklist3").tablesorter();
 });
 
-// Tablesorter
